b6997af700906cb6c3e24efb3b8250a0de543db0
EfimowEgor <egor.efimowA@yandex.ru>
2022-02-03 15:13:40 +0500

    Добавьте файлы проекта.

---
+++ Money.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab9
+{
+    public class Money
+    {
+        int rub, kop;
+        static int quantity = 0;
+        public int Rub
+        {
+            get 
+            { 
+                return rub; 
+            }
+            set
+            {
+                if (value < 0)
+                {
+                    Console.WriteLine("Количество рублей не может быть меньше 0");
+                    rub = 0;
+                }
+                else
+                    rub = value; 
+            }
+        }
+        public int Kop
+        {
+            get 
+            { 
+                return kop; 
+            }
+            set 
+            {
+                if (value < 0)
+                {
+                    Console.WriteLine("Число копеек не может быть меньше 0");
+                    kop = 0;
+                }
+                else
+                {
+                    if (value > 99)
+                    {
+                        rub += value / 100;
+                        kop = value % 100;
+                    }
+                    else
+                        kop = value;
+                }
+            }
+        }
+        public static int GetQuant
+        {
+            get
+            {
+                return quantity;
+            }
+        }
+        public Money()
+        {
+            rub = 0;
+            kop = 0;
+            quantity += 1;
+        }
+        public Money(int rb, int kp)
+        {
+            Rub = rb;
+            Kop = kp;
+            quantity += 1;
+        }
+        public void ShowValues()
+        {
+            Console.WriteLine($"У вас {Rub} рублей и {Kop} копеек");
+        }
+        // Статическая функция 
+        public static void Minus(Money m1, Money m2)
+        {
+            int sumKop1 = m1.Rub * 100 + m1.Kop;
+            int sumKop2 = m2.Rub * 100 + m2.Kop;
+            if(sumKop1 >= sumKop2)
+            {
+                m1.Rub = (sumKop1 - sumKop2) / 100;
+                m1.Kop = (sumKop1 - sumKop2) % 100;
+            }
+            else
+            {
+                Console.WriteLine("Денежная разность не может быть меньше 0. Уменьшаемое остаётся без изменений.");
+            }
+        }
+        // Метод класса
+        public void Minus(Money m2)
+        {
+            int sumKopMethod = this.Rub * 100 + this.Kop;
+            int sumM2 = m2.Rub * 100 + m2.Kop;
+            if (sumKopMethod >= sumM2)
+            {
+                this.Rub = (sumKopMethod - sumM2) / 100;
+                this.Kop = (sumKopMethod - sumM2) % 100;
+            }
+            else
+            {
+                Console.WriteLine("Денежная разность не может быть меньше 0. Уменьшаемое остаётся без изменений.");
+            }
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Lab9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Статическая функция 
+            Money acc1 = new Money(0, 0);
+            acc1.ShowValues();
+            Money acc2 = new Money(0, 0);
+            acc2.ShowValues();
+            // В результате вычитания изменяется Money mn1
+            Money.Minus(acc1, acc2);
+            acc1.ShowValues();
+
+            Console.WriteLine();
+
+            // Метод класса
+            Money acc3 = new Money(-12, 0);
+            acc3.ShowValues();
+            Money acc4 = new Money(4, 93);
+            acc4.ShowValues();
+            acc3.Minus(acc4);
+            acc3.ShowValues();
+
+            Console.WriteLine(Money.GetQuant);
+
+        }
+    }
+}
